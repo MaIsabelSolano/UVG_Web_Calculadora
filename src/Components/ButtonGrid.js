eca59f154a5b3735b9c889688d30c4a3a4c01650
Isabel Solano <misolanob@gmail.com>
2022-05-17 23:03:14 -0600

    Display semi-funcional, números funcionales y clear

--- src/Components/ButtonGrid.js
+++ src/Components/ButtonGrid.js
@@ -7,39 +7,59 @@ const Calculadora = () => {
     const [valor2, setValor2] = useState('')
     const [primero, setPrimero] = useState(true)
     const [disp, setDisplay] = useState('0')
+    const [operacion, setOperacion] =useState('')
 
     const addDigit = (digito) => {
         if (primero) {
-            let temp = valor1 + digito
-            setValor1(temp)
+            if (valor1.length <= 9){
+                let temp = valor1 + digito
+                setValor1(temp)
+                console.log(valor1)
+                setDisplay(valor1)
+            }
+            
         } else {
-            let temp = valor2 + digito
-            setValor2(temp)
+            if (valor2.length <= 9){
+                let temp = valor2 + digito
+                setValor2(temp)
+                console.log(valor2)
+                setDisplay(valor2)
+            }
         }
+    }
+
+    const clear = () => {
+        setValor1('')
+        setValor2('')
+        setDisplay('0')
+        setOperacion('')
+        setPrimero(true)
     }
 
     return (
         <div className="container">
-            <div className="disp">{disp}</div>
+            <div className="disp">
+                {disp}
+            </div>
             <div className="btnGrid">
-                <button className="upper">C</button>
-                <button className="upper">#</button>
+                <button className="upper" onClick={() => clear()}>C</button>
+                <button className="upper">⁺∕₋</button>
                 <button className="upper">%</button>
                 <button className="operation">/</button>
-                <button className="numeric" onClick={addDigit('7')}>7</button>
-                <button className="numeric">8</button>
-                <button className="numeric">9</button>
+                <button className="numeric" onClick={() => addDigit('7')}>7</button>
+                <button className="numeric" onClick={() => addDigit('8')}>8</button>
+                <button className="numeric" onClick={() => addDigit('9')}>9</button>
                 <button className="operation">x</button>
-                <button className="numeric">4</button>
-                <button className="numeric">5</button>
-                <button className="numeric">6</button>
+                <button className="numeric" onClick={() => addDigit('4')}>4</button>
+                <button className="numeric" onClick={() => addDigit('5')}>5</button>
+                <button className="numeric" onClick={() => addDigit('6')}>6</button>
                 <button className="operation">+</button>
-                <button className="numeric">1</button>
-                <button className="numeric">2</button>
-                <button className="numeric">3</button>
+                <button className="numeric" onClick={() => addDigit('1')}>1</button>
+                <button className="numeric" onClick={() => addDigit('2')}>2</button>
+                <button className="numeric" onClick={() => addDigit('3')}>3</button>
                 <button className="operation">-</button>
-                <button className="numeric zero">0</button>
-                <button className="numeric">.</button>
+                <button className="numeric zero" onClick={() => addDigit('0')}>0</button>
+                <button className="numeric" onClick={() => addDigit('.')}>.</button>
                 <button className="operation">=</button>
             </div>
         </div>
